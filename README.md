aedc9c5c177b17b6b25b3536e4c8a8ab3bba3d35
sunyi00 <sunyi00@gmail.com>
2017-12-20 18:31:28 +0800

    ein hello world

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # hello-world
 
-一个简单的 LAIN 应用，提供 HTTP 服务，请求 `/` 时返回 `Hello, LAIN. I'm container: $containerID. You are the ${n}th visitor.`。
-
-> - ${n} 指调用次数。
-> - 用到了 [LAIN Service](https://laincloud.gitbooks.io/white-paper/usermanual/service.html)。
-
-## 分支说明
-
-本仓库有多个分支，便于 LAIN 的逐步进阶演示。对应的演示步骤见 [LAIN Tutorial](https://laincloud.gitbooks.io/white-paper/content/tutorial/)。
-
-`basic`: 基础版，只展示 LAIN 最基本的功能。
-
-`service`: 进阶版，用 Redis 演示了 service 功能。
-
-`master`: 高级版，增加了`prepare`等 `lain.yaml` 的其它配置。
+一个用于演示的 LAIN 应用，提供 HTTP 服务，请求 `/` 时返回 `Hello, world! I'm container: $containerID.`。
--- main.go
+++ main.go
@@ -1,41 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
+	"fmt"
 	"os"
-
-	"github.com/go-redis/redis"
 )
 
-const visitCountKey = "visitCountKey"
-
 func main() {
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: "redis:6379",
-	})
 	containerID, _ := os.Hostname()
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if err := redisClient.Incr(visitCountKey).Err(); err != nil {
-			fmt.Fprintf(w, "redisClient.Incr() failed, error: %s.", err)
-			return
-		}
-
-		visitCountStr, err := redisClient.Get(visitCountKey).Result()
-		if err != nil {
-			fmt.Fprintf(w, "redisClient.Get() failed, error: %s.", err)
-			return
-		}
-
-		visitCount, err := strconv.Atoi(visitCountStr)
-		if err != nil {
-			fmt.Fprintf(w, "strconv.Atoi() failed, error: %s.", err)
-			return
-		}
-		fmt.Fprintf(w, "Hello, LAIN. I'm container: %s. " +
-		               "You are the %dth visitor.", containerID, visitCount)
+		fmt.Fprintf(w, "Hello, world! I'm container: %s.\n", containerID)
 	})
 
 	http.ListenAndServe(":8080", nil)
